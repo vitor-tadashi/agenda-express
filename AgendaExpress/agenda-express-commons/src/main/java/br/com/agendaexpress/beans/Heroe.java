8071ef9e421cdd6e077b1421e75868ace5fed0a5
Genka <xande.genka@gmail.com>
2015-07-11 20:04:29 -0300

    Alteração nome

---
+++ AgendaExpress/agenda-express-commons/src/main/java/br/com/agendaexpress/beans/Heroe.java
@@ -0,0 +1,33 @@
+package br.com.agendaexpress.beans;
+
+
+
+public class Heroe {
+	private Long id;
+	private String nome;
+
+	public Heroe() {
+	}
+
+	public Heroe(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
